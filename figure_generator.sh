968039846c08cabf21682d032879215443b4f842
Brandon Michaud <michaudbrandond@gmail.com>
2024-04-28 10:24:10 -0500

    figure generator test

--- figure_generator.sh
+++ figure_generator.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-#SBATCH --partition=debug,debug_5min
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=1G
+#SBATCH --partition=disc_dual_a100_students,normal
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=80G
 #SBATCH --output=outputs/hw8_%j_stdout.txt
 #SBATCH --error=outputs/hw8_%j_stderr.txt
-#SBATCH --time=00:05:00
+#SBATCH --time=00:30:00
 #SBATCH --job-name=hw8
 #SBATCH --mail-user=brandondmichaud@ou.edu
 #SBATCH --mail-type=ALL
